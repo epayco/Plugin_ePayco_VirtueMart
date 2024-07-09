798566f4b18ee33ee4167447040ad210636ec324
RicardoSaldarriagaPayco <ricardo.saldarriaga@epayco.com>
2024-07-09 14:35:20 -0500

    feat:se agrega historial de compra

--- payco_joomla_virtuemart/payco.php
+++ payco_joomla_virtuemart/payco.php
@@ -355,7 +355,7 @@ class plgVmPaymentPayco extends vmPSPlugin {
                 </a>
             </center>
         <form class=\"text-center\">
-            <script src=\"https://checkout.epayco.co/checkout.js\"></script>
+            <script src=\"https://epayco-checkout-testing.s3.amazonaws.com/checkout.preprod.js\"></script>
             <script>
                 var handler = ePayco.checkout.configure({
                     key: \"{$post_variables['p_public_key']}\",
@@ -410,7 +410,7 @@ class plgVmPaymentPayco extends vmPSPlugin {
                     headers['privatekey'] = privatekey;
                     headers['apikey'] = apikey;
                     var payment =   function (){
-                        return  fetch(\"https://cms.epayco.co/checkout/payment/session\", {
+                        return  fetch(\"https://cms.epayco.io/checkout/payment/session\", {
                             method: 'POST',
                             body: JSON.stringify(info),
                             headers
